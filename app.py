a462ea44f96f5ad30a79a9a89b1b0b1f66c200ed
dwy <ybh6954@gmail.com>
2024-07-04 12:43:25 +0900

    SEXY CUTE IIII EEEE 추가

--- app.py
+++ app.py
@@ -19,77 +19,191 @@ class User(db.Model):
     password = db.Column(db.String(150), nullable=False)
     has_voted = db.Column(db.Boolean, default=False)
 
+class Vote(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    voter_name = db.Column(db.String(150), nullable=False)
+    name = db.Column(db.String(150), nullable=False)
+    mbti = db.Column(db.String(10), nullable=False)
+
 NAMES = ["박정호", "류범상", "김경민", "유동원", "이은경", "김가은", "한경훈", "배재형", "공준식", "김태영"]
-MBTIS = ["INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP", "CUTE", "SEXY"]
+MBTIS = ["INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP", "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP", "CUTE", "SEXY", "IIII", "EEEE"]
 
-votes = {name: {mbti: 0 for mbti in MBTIS} for name in NAMES}
-
-actual_mbti = {
-    "유동원": "INTJ",
-    "이은경": "ISFJ",
-    "김경민": "ISTJ",
-    "류범상": "INTP",
-    "박정호": "INTJ",
-    "김가은": "ENTJ",
-    "한경훈": "ISTJ",
-    "배재형": "ESFJ",
-    "공준식": "INTJ",
-    "김태영": "ISFJ"
-}
-vote_details = []
+actual_mbti = {"유동원": "INTJ",
+               "이은경": "ISFJ",
+               "김경민": "ISTJ",
+               "류범상": "INTP",
+               "박정호": "INTJ",
+               "김가은": "ENTJ",
+               "한경훈": "ISTJ",
+               "배재형": "ESFJ",
+               "공준식": "INTJ",
+               "김태영": "ISFJ"
+            }
 
 start_time = datetime.now()
 end_time = start_time + timedelta(hours=12)
 
+color_map = {
+    "INTJ": "#FF5733",
+    "INTP": "#33FF57",
+    "ENTJ": "#3375FF",
+    "ENTP": "#FF33A6",
+    "INFJ": "#75FF33",
+    "INFP": "#FF3333",
+    "ENFJ": "#FFA633",
+    "ENFP": "#FF3333",
+    "ISTJ": "#A6FF33",
+    "ISFJ": "#33FF75",
+    "ESTJ": "#3375FF",
+    "ESFJ": "#A633FF",
+    "ISTP": "#FF33A6",
+    "ISFP": "#FF5733",
+    "ESTP": "#33FFF7",
+    "ESFP": "#A6FF33",
+    "CUTE": "#FF33A6",
+    "SEXY": "#FF5733",
+    "IIII": "#FF3333",
+    "EEEE": "#33FF75"
+}
+
 @app.route('/')
 def index():
-    user_votes = [vote_detail['name'] for vote_detail in vote_details if vote_detail['voterName'] == session.get('username')]
-    return render_template('index.html', user_votes=user_votes if user_votes else [], end_time=end_time.timestamp(), names=NAMES, mbtis=MBTIS)
+    if 'username' not in session:
+        return redirect(url_for('login'))
+    session.permanent = True  # 세션을 영구적으로 설정하여 세션 만료 시간을 적용합니다.
+    user_votes = Vote.query.filter_by(voter_name=session['username']).all()
+    user_votes_names = [vote.name for vote in user_votes]
+    return render_template('index.html', username=session['username'], user_votes=user_votes_names, end_time=end_time.timestamp(), names=NAMES, mbtis=MBTIS)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            flash('이미 가입된 아이디입니다.', 'error')
+            return redirect(url_for('register'))
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')
+        
+        new_user = User(username=username, password=hashed_password)
+        try:
+            db.session.add(new_user)
+            db.session.commit()
+            flash('회원가입을 축하합니다', 'success')
+            flash(username, 'username')
+            flash(password, 'password')
+            return redirect(url_for('register'))
+        except Exception as e:
+            db.session.rollback()
+            flash(f'회원가입 중 오류가 발생했습니다: {str(e)}', 'error')
+    
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        
+        if not user:
+            flash('없는 아이디입니다. 회원가입을 해주세요', 'error')
+        elif not check_password_hash(user.password, password):
+            flash('비밀번호가 틀렸습니다. 재입력해주세요. 기억이 안나시면 관리자에게 문의해주세요', 'error')
+        else:
+            session['username'] = username
+            session['has_voted'] = user.has_voted
+            session.permanent = True  # 세션을 영구적으로 설정하여 세션 만료 시간을 적용합니다.
+            return redirect(url_for('index'))
+    
+    return render_template('login.html')
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+    session.pop('username', None)
+    session.pop('has_voted', None)
+    return jsonify({'status': 'success'})
 
 @app.route('/vote', methods=['POST'])
 def vote():
+    if 'username' not in session:
+        return redirect(url_for('login'))
+
     data = request.json
     voter_name = data['voterName']
     name = data['name']
     mbti = data['mbti']
     
+    if not voter_name:
+        return jsonify({'status': 'fail', 'message': '투표자 이름을 입력해주세요.'})
+    
     # Check if the user has already voted for this person
-    for vote_detail in vote_details:
-        if vote_detail['voterName'] == voter_name and vote_detail['name'] == name:
-            return jsonify({'status': 'fail', 'message': '이미 해당 사람에게 투표하셨습니다!'})
+    existing_vote = Vote.query.filter_by(voter_name=voter_name, name=name).first()
+    if existing_vote:
+        return jsonify({'status': 'fail', 'message': '이미 해당 사람에게 투표하셨습니다!'})
 
-    votes[name][mbti] += 1
-    vote_details.append({'voterName': voter_name, 'name': name, 'mbti': mbti})
+    if mbti in ["CUTE", "SEXY", "IIII", "EEEE"]:
+        return jsonify({'status': 'fail', 'message': f"'{voter_name}' 님, '{name}' 이(가) {mbti} 한 건 알지만 {mbti}는 MBTI 가 아닙니다"})
 
+    new_vote = Vote(voter_name=voter_name, name=name, mbti=mbti)
+    db.session.add(new_vote)
+    db.session.commit()
     return jsonify({'status': 'success', 'message': '투표가 성공적으로 완료되었습니다.'})
+
+@app.route('/delete_vote', methods=['POST'])
+def delete_vote():
+    if 'username' not in session:
+        return jsonify({'status': 'fail', 'message': '로그인이 필요합니다.'})
+
+    data = request.json
+    voter_name = data['voterName']
+    name = data['name']
+
+    Vote.query.filter_by(voter_name=voter_name, name=name).delete()
+    db.session.commit()
+    
+    return jsonify({'status': 'success', 'message': '재투표가 가능하게 되었습니다.'})
 
 @app.route('/results', methods=['GET'])
 def results():
+    votes = {name: {mbti: 0 for mbti in MBTIS} for name in NAMES}
+    all_votes = Vote.query.all()
+    for vote in all_votes:
+        votes[vote.name][vote.mbti] += 1
+
     fig, axes = plt.subplots(3, 3, figsize=(15, 15))
     axes = axes.flatten()
 
     for ax, (name, mbti_votes) in zip(axes, votes.items()):
         labels = [mbti for mbti, count in mbti_votes.items() if count > 0]
         sizes = [count for count in mbti_votes.values() if count > 0]
+        colors = [color_map[mbti] for mbti in labels]
+
         if sum(sizes) == 0:
             ax.text(0.5, 0.5, '아직 투표수가 부족합니다', horizontalalignment='center', verticalalignment='center', transform=ax.transAxes)
             ax.set_title(name, fontsize=12)
             ax.axis('off')
         else:
-            ax.pie(sizes, labels=labels, autopct='%1.1f%%')
+            ax.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%')
             ax.set_title(name, fontsize=12)
 
     for ax in axes[len(votes):]:
         ax.axis('off')
 
     plt.tight_layout()
-    img = io.BytesIO()  # 수정된 부분
+    img = io.BytesIO()
     plt.savefig(img, format='png')
     img.seek(0)
     return send_file(img, mimetype='image/png')
 
 @app.route('/compare')
 def compare():
+    votes = {name: {mbti: 0 for mbti in MBTIS} for name in NAMES}
+    all_votes = Vote.query.all()
+    for vote in all_votes:
+        votes[vote.name][vote.mbti] += 1
+
     results = {}
     show_results = False
     for name, mbti_votes in votes.items():
@@ -122,22 +236,7 @@ def admin():
 def vote_details_page():
     if not session.get('admin'):
         return redirect(url_for('admin'))
-    return render_template('vote_details.html', vote_details=vote_details, actual_mbti=actual_mbti)
-
-@app.route('/delete_vote', methods=['POST'])
-def delete_vote():
-    if 'username' not in session:
-        return jsonify({'status': 'fail', 'message': '로그인이 필요합니다.'})
-
-    data = request.json
-    voter_name = data['voterName']
-    name = data['name']
-
-    global vote_details
-    vote_details = [vote_detail for vote_detail in vote_details if not (vote_detail['voterName'] == voter_name and vote_detail['name'] == name)]
-    
-    votes[name] = {mbti: 0 for mbti in votes[name].keys()}  # 해당 이름의 모든 MBTI 투표를 초기화합니다.
-    return jsonify({'status': 'success', 'message': '재투표가 가능하게 되었습니다.'})
+    return render_template('vote_details.html', vote_details=Vote.query.all(), actual_mbti=actual_mbti)
 
 @app.route('/update_mbti', methods=['POST'])
 def update_mbti():
@@ -148,7 +247,20 @@ def update_mbti():
     actual_mbti[name] = data['mbti']
     return redirect(url_for('vote_details_page'))
 
+@app.route('/clear_votes', methods=['POST'])
+def clear_votes():
+    if not session.get('admin'):
+        return redirect(url_for('admin'))
+    try:
+        Vote.query.delete()
+        db.session.commit()
+        flash('모든 투표 데이터가 초기화되었습니다.', 'success')
+    except Exception as e:
+        db.session.rollback()
+        flash(f'투표 데이터 초기화 중 오류가 발생했습니다: {str(e)}', 'error')
+    return redirect(url_for('vote_details_page'))
+
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # 데이터베이스와 테이블을 생성합니다.
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
